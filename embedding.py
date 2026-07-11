8993e024a13b3de5db67fcb679d6c89cc0a552a0
rsandha19 <rsingh5@mcneese.edu>
2026-07-11 01:01:07 -0500

    refactored code into two working functions : embed_and_chunk() and dense_search() in order to be used for BM25 searcha and reranking

--- embedding.py
+++ embedding.py
@@ -1,21 +1,25 @@
 from sentence_transformers import SentenceTransformer
 from Chunk import chunk_contract
+import chromadb
 with open("data/processed/sample_contract.txt","r",encoding="utf-8") as f:
     test_case=f.read()
 chunks=chunk_contract(test_case,5,1)
-model=SentenceTransformer('all-MiniLM-L6-v2')
-chunk_vectors=model.encode(chunks)
 
-import chromadb
+model=SentenceTransformer('all-MiniLM-L6-v2')
 client=chromadb.PersistentClient(path="data/processed/chroma_db")
 collection=client.get_or_create_collection(name="legal_contracts")
-embeddings_list=chunk_vectors.tolist()
-collection.add(embeddings=embeddings_list,
+
+def embed_and_chunk(chunks,model,collection):
+    chunk_vectors=model.encode(chunks)
+    embeddings_list=chunk_vectors.tolist()
+    collection.add(embeddings=embeddings_list,
                documents=chunks,
                ids=[f"chunk_{i}" for i in range (0,chunk_vectors.shape[0])])
 
-test_string="can the tenenat terminate the lease early?"
-test_embedding=[1]
-test_embedding[0]=model.encode(test_string)
+query="can the tenenat terminate the lease early?"
+def dense_search(query,model,collection,n_results=5):
+    test_embedding=[model.encode(query)]
+    return(collection.query(query_embeddings=test_embedding,n_results=n_results))
 
-print(collection.query(query_embeddings=test_embedding,n_results=3))
+embed_and_chunk(chunks,model,collection)
+print(dense_search(query,model,collection))
